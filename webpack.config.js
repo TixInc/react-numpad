5238e639aa75e84c5539b621deda70910cc87102
Garret LeCompte <garret.lecompte@tix.com>
2026-04-23 11:23:40 -0700

    package updates

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,39 @@ module.exports = {
   },
   mode: 'development',
   resolve: {
-    modules: ['node_modules', 'bower_components'],
+    modules: ['node_modules'],
+    extensions: [ '.js', '.jsx']
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader', // 'babel-loader' is also a valid name to reference
-        query: {
-          presets: ['es2015', 'react'],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader', 
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, 
+        use: ['style-loader', 'css-loader'] 
+      },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
-      { test: /\.(png|jpg|)$/, loader: 'url-loader?limit=200000' },
+      { test: /\.(png|jpg|)$/, 
+        loader: 'url-loader',
+        options: {
+          limit: 200000,
+        },
+      },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+        },
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -36,7 +48,7 @@ module.exports = {
       },
       {
         test: /node_modules\/(pdfkit|fontkit|png-js|linebreak|unicode-properties|brotli)\//,
-        loader: 'transform-loader?brfs',
+        loader: 'transform-loader',
       },
       { test: /node_modules\/unicode-properties.*\.json$/, use: 'json-loader' },
     ],
